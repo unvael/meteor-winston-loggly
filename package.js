bf2680c5e7b4774a4c5e7f0246d426494b352109
Jonathan Rhone <rhone.j@gmail.com>
2013-12-25 02:30:44 -0500

    enable access to npm modules on the server, up version of winston-loggly

--- package.js
+++ package.js
@@ -3,13 +3,17 @@ Package.describe({
 });
 
 Npm.depends({
-    "winston": "0.7.2",
-    "winston-loggly": "0.6.0"
+  "winston": "0.7.2",
+  "winston-loggly": "1.0.0"
 });
 
 Package.on_use(function (api, where) {
-  api.add_files('winston-loggly.js', 'server');
+  api.use(['winston'], 'server');
+  api.use(['winston-loggly'], 'server');
+
   if(api.export){
     api.export('Winston_Loggly');
   }
+  api.add_files('winston-loggly.js', 'server');
+
 });

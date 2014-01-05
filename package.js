fa8578afe4262ed31a14ac2c607975e41618ce6e
Jonathan Rhone <rhone.j@gmail.com>
2014-01-04 20:00:01 -0500

    importing npm dependencies fix

--- package.js
+++ package.js
@@ -2,14 +2,14 @@ Package.describe({
   summary: 'A Loggly transport for winston.'
 });
 
-Npm.depends({
-  "winston": "0.7.2",
-  "winston-loggly": "1.0.0"
-});
-
 Package.on_use(function (api, where) {
   api.use(['winston'], 'server');
   api.use(['winston-loggly'], 'server');
+
+  Npm.depends({
+    "winston": "0.7.2",
+    "winston-loggly": "1.0.0"
+  });
 
   if(api.export){
     api.export('Winston_Loggly');

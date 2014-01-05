0195de324c4f6462b6d0c4946e56854abe829750
Jonathan Rhone <rhone.j@gmail.com>
2014-01-04 20:26:33 -0500

    fixing package.js

--- package.js
+++ package.js
@@ -1,19 +1,11 @@
+
 Package.describe({
   summary: 'A Loggly transport for winston.'
 });
 
 Package.on_use(function (api, where) {
-  api.use(['winston'], 'server');
-  api.use(['winston-loggly'], 'server');
-
   Npm.depends({
     "winston": "0.7.2",
     "winston-loggly": "1.0.0"
   });
-
-  if(api.export){
-    api.export('Winston_Loggly');
-  }
-  api.add_files('winston-loggly.js', 'server');
-
 });
